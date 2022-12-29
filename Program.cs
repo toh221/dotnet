6121b7f934721b393338cdc9fad5de03d3203673
TH <118343638+toh221@users.noreply.github.com>
2022-12-29 13:57:51 +0100

    add: grundgeruest

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 
+/************************************* 
+Fertigstellung:   <Datum, wann Sie fertig geworden sind> 
+Bearbeiter(in) 1:  Vo, Felix, 1347526
+Bearbeiter(in) 2:  Hüben, Tobias, 1370737
+***************************************/ 
+/*
+namespace bwi40322 {
+    class Program {
+
+    }
+}
