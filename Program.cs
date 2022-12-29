dd9d2d1ac72cb391c8fa622e5e6c410266678bb8
TH <118343638+toh221@users.noreply.github.com>
2022-12-29 14:00:47 +0100

    add: minimalkommentierung

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ Fertigstellung:   <Datum, wann Sie fertig geworden sind>
 Bearbeiter(in) 1:  Vo, Felix, 1347526
 Bearbeiter(in) 2:  Hüben, Tobias, 1370737
 ***************************************/ 
-/*
+
 namespace bwi40322 {
     class Program {
+
+        /* -------------------------------------------------- 
+   Stage 1: Daten vom Webserver holen 
+-----------------------------------------------------*/ 
+ 
+/* -------------------------------------------------- 
+   Stage 2: Daten von Lokal laden und aufbereiten 
+-----------------------------------------------------*/ 
+ 
+/* -------------------------------------------------- 
+   Stage 3: Daten transformieren, Kennzahl(en) erzeugen 
+-----------------------------------------------------*/ 
+ 
+/* -------------------------------------------------- 
+   Stage 4: Ausgaben / Visualisierung erzeugen und speichern 
+-----------------------------------------------------*/
 
     }
 }
