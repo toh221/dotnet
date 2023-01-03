513c76f663f6ac13a367440e6fcb5ffabcc82344
Fel1xVo <felix.vo@outlook.com>
2023-01-03 22:19:02 +0100

    fertig

--- Program.cs
+++ Program.cs
@@ -90,16 +90,40 @@ writer.Close();
 /* -------------------------------------------------- 
    Stage 4: Ausgaben / Visualisierung erzeugen und speichern 
 -----------------------------------------------------*/
-foreach(string zeitpunkt in zeitpunkt)
-   if(string.IsNullOrEmpty(zeitpunkt)) {
+HtmlDocument htmlDoc = new HtmlDocument();
 
-   }
-   else {
-      double value = double.Parse(zeitpunkt);
+int[] strommittel = new int[10,2];
 
-   }
+// Lade die HTML-Datei
+htmlDoc.Load("index.html");
 
-        }
-        }
+// Suche das ul-Element mit der Klasse "container"
+HtmlNode ulNode = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class='container']");
+
+if (ulNode != null)
+{  
+   foreach(int daten in strommittel)
+    // Erstelle ein neues li-Element mit dem Wert "300" und dem data-cp-size Attribut
+    HtmlNode liNode = HtmlNode.CreateNode("<li data-cp-size='" +strommittel[daten,1]+ "'>" + strommitel[daten,1] + "</li>");
+
+    // Füge das li-Element als letztes Kind des ul-Elements hinzu
+    ulNode.AppendChild(liNode);
 }
+
+// Suche das div-Element mit der Klasse "x-axis"
+HtmlNode divNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='x-axis']");
+
+if (divNode != null)
+{
+    // Erstelle ein neues span-Element mit dem Wert "10"
+    HtmlNode spanNode = HtmlNode.CreateNode("<span>"+ strommittel[daten,2] +"</span>");
+
+    // Füge das span-Element als letztes Kind des div-Elements hinzu
+    divNode.AppendChild(spanNode);
+}
+// Speichere die HTML-Datei
+htmlDoc.Save("index.html");
+        }
+        }
+        }
 
